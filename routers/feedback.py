4864c4e7a48d3ff2ef413af37a4701392e25255e
Shubham Kumar <shubhamkumarjhadss8@gmail.com>
2025-03-07 01:04:46 +0530

    Add feedback router and main application structure

---
+++ routers/feedback.py
@@ -0,0 +1,39 @@
+from fastapi import Form, APIRouter 
+from typing import Annotated
+from pydantic import BaseModel
+import os
+from google import genai
+
+router = APIRouter()
+# Taking the question, answer and keypoints as input
+class FormData(BaseModel):
+    question: str
+    answer: str
+    keypoints: str
+
+# Creating a client object
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# Endpoint to give feedback
+@router.post("/feedback")
+def give_feedback(data: Annotated[FormData, Form()]):
+# Prompt for the model
+    prompt = f"""I am sharing you a question and the answer given by the student.
+
+            And the key points by teacher to be there in the answer.
+            
+            Give me a short feedback directly without any greeting or introduction.
+            Don't give me the answer i only need feedback as you are telling this to a student.
+            
+            Question: {data.question}
+            Answer given by student: {data.answer}
+            Key points by teacher: {data.keypoints}
+            """
+    # Generating the content
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+    )
+    # print(data)
+    # print(response.text)
+    return response.text
