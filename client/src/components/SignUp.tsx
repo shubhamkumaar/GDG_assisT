6e51718f16b0e57122dcb6a270646d4f5b844749
Ritvik Anand <128292481+RitvikAnand583@users.noreply.github.com>
2025-03-25 01:52:35 +0530

    login and logout

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -60,7 +60,7 @@ export default function SignUpPage() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="mt-1 appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                placeholder="John Doe"
+                placeholder="Full Name"
               />
             </div>
             <div>
